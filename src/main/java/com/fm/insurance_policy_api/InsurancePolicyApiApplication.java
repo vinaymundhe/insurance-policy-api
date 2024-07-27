79a7dc0d48e54d575170143bd34db2b112d7f6e8
Vinay Mundhe <vinay.mundhe19@gmail.com>
2024-07-28 01:27:08 +0530

    Initial commit with Spring Boot setup

---
+++ src/main/java/com/fm/insurance_policy_api/InsurancePolicyApiApplication.java
@@ -0,0 +1,13 @@
+package com.fm.insurance_policy_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsurancePolicyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsurancePolicyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fm/insurance_policy_api/InsurancePolicyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fm.insurance_policy_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsurancePolicyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
